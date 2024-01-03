d3d80845e00cebde453ac3438dbb3f39c9100bf0
Benjamin Jarrell <benjaminejarrell@gmail.com>
2024-01-03 13:01:36 -0600

    add support for static redirects

--- cloudfront_function.js
+++ cloudfront_function.js
@@ -1,23 +1,23 @@
 function handler(event) {
     var request = event.request;
 
-    var destination = "{destination}" //{destination} is replaced by Terraform
-    var isStaticRedirect = false
+    var destination = "{destination}"; //{destination} is replaced by Terraform
+    var isStaticRedirect = "{is_static_redirect}"; //{is_static_redirect} is replaced by Terraform
 
-    if(!isStaticRedirect) { //Add the request uri unless it's a static redirect
-        var locationHeader = destination + request.uri
+    if (isStaticRedirect) { //Add the request uri unless it's a static redirect
+        var locationHeader = destination;
     }
     else {
-        var locationHeader = destination
+        var locationHeader = destination + request.uri;
     }
 
     var response = {
         statusCode: 301,
         statusDescription: 'Moved Permanently',
         headers: {
-            "location": {"value": locationHeader }
+            "location": { "value": locationHeader }
         }
-    }
+    };
 
     return response;
 }
