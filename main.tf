045acac1620425438837c5f0ef00cd11aaf18b75
Benjamin Jarrell <benjaminejarrell@gmail.com>
2024-06-11 13:07:57 -0500

    add ipv6 support

--- main.tf
+++ main.tf
@@ -114,6 +114,19 @@ resource "aws_route53_record" "cloudfront" {
   }
 }
 
+resource "aws_route53_record" "cloudfront_ipv6" {
+  zone_id         = var.route53_zone_id
+  name            = var.domain
+  type            = "AAAA"
+  allow_overwrite = false
+
+  alias {
+    name                   = aws_cloudfront_distribution.main.domain_name
+    zone_id                = aws_cloudfront_distribution.main.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
 resource "aws_route53_record" "validation" {
   for_each = {
     for dvo in aws_acm_certificate.cloudfront.domain_validation_options : dvo.domain_name => {
